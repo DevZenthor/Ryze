a9cb9f07c004522baebbf70a0f66e303c463a38d
DevZenthor <zenthorpro@outlook.com>
2026-03-23 14:28:36 +0100

    feat : add background

---
+++ portfolio-monteur/src/App.jsx
@@ -0,0 +1,15 @@
+import LogoBackground from "./components/LogoBackground"
+
+export default function App() {
+  return (
+    <div className="relative min-h-screen text-white">
+
+      <LogoBackground />
+
+      <main className="relative z-10 flex items-center justify-center min-h-screen">
+     
+      </main>
+
+    </div>
+  )
+}
